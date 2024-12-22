c1dab3d07969f8a3db61693933d46680e0dca915
Thomas Brockington <xtombrock1989@gmail.com>
2024-12-22 16:51:49 +0000

    init

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,13 +1,10 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 // Analytics
 import { usePageTracking } from '../../hooks/useAnalytics';
 // Constants
 import { CompanyName } from '../../utils/Constants';
 // Components
-import Navbar from '../../components/nav/Navbar';
 import { HelmetItem } from '../../components/utils/HelmetItem';
-import FooterComponent from '../../components/footer/FooterComponent';
-import HomePageMainContainer from '../../components/home/HomePageMainContainer';
 // Data
 import {
   homePageAdditionalMeta,
@@ -23,24 +20,14 @@ const HomePage = React.memo(() => {
       {/* Tab Data */}
       <HelmetItem
         PageName='Home'
-        desc={`${CompanyName} offers expert web and circuit design solutions in England. Discover our services and featured projects.`}
-        keywords={`web design, circuit design, ${CompanyName}, England, UK, custom solutions`}
+        desc={`${CompanyName}`}
+        keywords={``}
         additionalMeta={homePageAdditionalMeta}
         structuredData={homePageStructuredData}
       />
 
       {/* Page */}
       <div className='grid min-h-screen lg:h-screen lg:max-h-screen lg:overflow-hidden bg-main-background font-poppins'>
-        <div className='grid grid-rows-reg'>
-          {/* Navigation */}
-          <Navbar />
-
-          {/* Main page content */}
-          <HomePageMainContainer />
-
-          {/* Footer */}
-          <FooterComponent />
-        </div>
       </div>
     </>
   );
